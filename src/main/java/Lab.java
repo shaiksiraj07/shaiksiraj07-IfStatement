c1f5633249bee91cb119a06186476092f88aaba2
shaiksiraj07 <shaiksiraj1015@gmail.com>
2025-09-24 19:46:18 +0530

    done

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,12 +20,7 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        if(bool){
-            return x;
-        }
-        else{
-            return y;
-        }
+        return bool ? x : y;
     }
     
 }
